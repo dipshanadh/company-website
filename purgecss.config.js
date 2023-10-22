b3724d5c22fcd35c17a1eb655493083cb883dc2e
Dipshan Adhikari <dipshanadh@gmail.com>
2023-10-22 12:32:06 +0545

    Configure purgeCSS

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   content: ["dist/**/*.html"], // The HTML files to analyze
-  css: ["dist/styles/styles.css"], // The CSS file to be purged
+  css: ["dist/styles/*.css"], // The CSS file to be purged
 };
